d52cdd2761a27dcedb3136c118fc52ce14c82beb
Anaelle EA <anaelle3364@gmail.com>
2023-12-15 16:51:19 +0100

    add name & email to dev entity

--- api-teamaker/src/main/java/org/teamaker/developer/domain/Developer.java
+++ api-teamaker/src/main/java/org/teamaker/developer/domain/Developer.java
@@ -1,4 +1,13 @@
 package org.teamaker.developer.domain;
 
+import java.util.ArrayList;
+
 public class Developer {
+    String fullName;
+    String email;
+
+    public Developer(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
 }
