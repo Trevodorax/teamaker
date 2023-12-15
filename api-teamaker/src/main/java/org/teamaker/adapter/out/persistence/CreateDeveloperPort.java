4cd7aaecffd6d258923e78782a7ce2de61fdd7a7
Anaelle EA <anaelle3364@gmail.com>
2023-12-15 16:51:52 +0100

    create CreateDeveloperPort

---
+++ api-teamaker/src/main/java/org/teamaker/adapter/out/persistence/CreateDeveloperPort.java
@@ -0,0 +1,7 @@
+package org.teamaker.adapter.out.persistence;
+
+import org.teamaker.developer.domain.Developer;
+
+public interface CreateDeveloperPort {
+    public boolean createDeveloper(Developer developer);
+}
