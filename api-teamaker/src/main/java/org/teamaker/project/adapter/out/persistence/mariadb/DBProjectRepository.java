4c966f61e0581936df58b81da0e00725ee037cee
Trevodorax <phvcgdx@gmail.com>
2023-12-15 20:42:52 +0100

    write tests for service

--- api-teamaker/src/main/java/org/teamaker/project/adapter/out/persistence/mariadb/DBProjectRepository.java
+++ api-teamaker/src/main/java/org/teamaker/project/adapter/out/persistence/mariadb/DBProjectRepository.java
@@ -1,9 +1,11 @@
 package org.teamaker.project.adapter.out.persistence.mariadb;
 
+import org.springframework.stereotype.Component;
 import org.teamaker.project.adapter.out.persistence.ProjectRepository;
 import org.teamaker.project.application.port.out.CreateProjectCommand;
 import org.teamaker.project.domain.Project;
 
+@Component
 public class DBProjectRepository implements ProjectRepository {
     @Override
     public Project createProject(CreateProjectCommand command) {
--- api-teamaker/src/main/java/org/teamaker/project/application/SubmitProjectService.java
+++ api-teamaker/src/main/java/org/teamaker/project/application/SubmitProjectService.java
@@ -3,14 +3,22 @@ package org.teamaker.project.application;
 import org.springframework.stereotype.Component;
 import org.teamaker.project.application.port.in.SubmitProjectCommand;
 import org.teamaker.project.application.port.in.SubmitProjectUseCase;
+import org.teamaker.project.application.port.out.CreateProjectCommand;
+import org.teamaker.project.application.port.out.CreateProjectPort;
 import org.teamaker.project.domain.Project;
 
 @Component
 class SubmitProjectService implements SubmitProjectUseCase {
+    private final CreateProjectPort createProjectPort;
+
+    public SubmitProjectService(CreateProjectPort createProjectPort) {
+        this.createProjectPort = createProjectPort;
+    }
+
     @Override
     public Project submitProject(SubmitProjectCommand command) {
-        // TODO: create the project
+        Project createdProject = createProjectPort.createProject(new CreateProjectCommand(command.getName(), command.getDescription(), command.getPriority(), command.getStartDate(), command.getEndDate()));
         // TODO: create the team and assign the devs to the project
-        return null;
+        return createdProject;
     }
 }
