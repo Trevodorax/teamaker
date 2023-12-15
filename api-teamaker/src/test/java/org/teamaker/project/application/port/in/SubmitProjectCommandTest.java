fb7fcf58240779f40e706fd75dbcd17369695777
Trevodorax <phvcgdx@gmail.com>
2023-12-15 18:22:39 +0100

    improve submit project command test

--- api-teamaker/src/test/java/org/teamaker/project/application/port/in/SubmitProjectCommandTest.java
+++ api-teamaker/src/test/java/org/teamaker/project/application/port/in/SubmitProjectCommandTest.java
@@ -12,59 +12,61 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubmitProjectCommandTest {
-    private static String name = "Project Name";
-    private static String description = "Project Description";
-    private static Priority priority = Priority.CRITICAL;
-    private static LocalDate startDate = LocalDate.now().plusDays(1);
-    private static LocalDate endDate = startDate.plusDays(5);
-    private static ArrayList<UUID> technologies = new ArrayList<>();
+    private static String validName = "Project Name";
+    private static String validDescription = "Project Description";
+    private static Priority validPriority = Priority.CRITICAL;
+    private static LocalDate validStartDate = LocalDate.now().plusDays(1);
+    private static LocalDate validEndDate = validStartDate.plusDays(5);
+    private static ArrayList<UUID> validTechnologies = new ArrayList<>();
+
     @BeforeAll
     public static void setUp() {
-        name = "Project Name";
-        description = "Project Description";
-        priority = Priority.CRITICAL;
-        startDate = LocalDate.now().plusDays(1);
-        endDate = startDate.plusDays(5);
-        technologies = new ArrayList<>();
-        technologies.add(UUID.randomUUID());
+        validName = "Project Name";
+        validDescription = "Project Description";
+        validPriority = Priority.CRITICAL;
+        validStartDate = LocalDate.now().plusDays(1);
+        validEndDate = validStartDate.plusDays(5);
+        validTechnologies = new ArrayList<>();
+        validTechnologies.add(UUID.randomUUID());
     }
+
     @Test
     public void testConstructorValidData() {
-        SubmitProjectCommand command = new SubmitProjectCommand(name, description, priority, startDate, endDate, technologies);
+        SubmitProjectCommand command = new SubmitProjectCommand(validName, validDescription, validPriority, validStartDate, validEndDate, validTechnologies);
 
-        assertEquals(name, command.getName());
-        assertEquals(description, command.getDescription());
-        assertEquals(priority, command.getPriority());
-        assertEquals(startDate, command.getStartDate());
-        assertEquals(endDate, command.getEndDate());
-        assertEquals(technologies, command.getTechnologies());
+        assertEquals(validName, command.getName());
+        assertEquals(validDescription, command.getDescription());
+        assertEquals(validPriority, command.getPriority());
+        assertEquals(validStartDate, command.getStartDate());
+        assertEquals(validEndDate, command.getEndDate());
+        assertEquals(validTechnologies, command.getTechnologies());
     }
 
     @Test
     public void testConstructorPastStartDate() {
         assertThrows(ConstraintViolationException.class, () -> {
-            new SubmitProjectCommand(name, description, priority, LocalDate.now().minusDays(1), endDate, technologies);
+            new SubmitProjectCommand(validName, validDescription, validPriority, LocalDate.now().minusDays(1), validEndDate, validTechnologies);
         });
     }
 
     @Test
     public void testConstructorEmptyName() {
         assertThrows(ConstraintViolationException.class, () -> {
-            new SubmitProjectCommand(null, description, priority, startDate, endDate, technologies);
+            new SubmitProjectCommand(null, validDescription, validPriority, validStartDate, validEndDate, validTechnologies);
         });
     }
 
     @Test
     public void testConstructorEmptyPriority() {
         assertThrows(ConstraintViolationException.class, () -> {
-            new SubmitProjectCommand(name, description, null, startDate, endDate, technologies);
+            new SubmitProjectCommand(validName, validDescription, null, validStartDate, validEndDate, validTechnologies);
         });
     }
 
     @Test
     public void testConstructorEndDateBeforeStartDate() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new SubmitProjectCommand(name, description, priority, startDate, startDate.minusDays(1), technologies);
+            new SubmitProjectCommand(validName, validDescription, validPriority, validStartDate, validStartDate.minusDays(1), validTechnologies);
         });
     }
 }
