bc8236c05c5a91b38661dd778894e29f05723074
Anaelle EA <anaelle3364@gmail.com>
2023-12-18 00:38:37 +0100

    create resign developer use case

---
+++ api-teamaker/src/main/java/org/teamaker/developer/application/port/in/ResignDeveloperUseCase.java
@@ -0,0 +1,7 @@
+package org.teamaker.developer.application.port.in;
+
+import java.util.Date;
+
+public interface ResignDeveloperUseCase {
+    public Date resignDeveloper(ResignDeveloperCommand command);
+}
