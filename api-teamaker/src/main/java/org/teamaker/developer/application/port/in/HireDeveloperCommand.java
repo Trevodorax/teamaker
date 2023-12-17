f2657d21678aff2fdf2b44fdf6133bde056b4c8f
Anaelle EA <anaelle3364@gmail.com>
2023-12-17 23:21:56 +0100

    add self validation to hire developer use case

--- api-teamaker/src/main/java/org/teamaker/developer/application/port/in/HireDeveloperCommand.java
+++ api-teamaker/src/main/java/org/teamaker/developer/application/port/in/HireDeveloperCommand.java
@@ -1,9 +1,11 @@
 package org.teamaker.developer.application.port.in;
 
+import org.teamaker.shared.validation.SelfValidating;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
-public class HireDeveloperCommand {
+public class HireDeveloperCommand extends SelfValidating<HireDeveloperCommand> {
     @NotNull
     private  final String fullName;
     @Email
@@ -12,6 +14,8 @@ public class HireDeveloperCommand {
     public HireDeveloperCommand(String fullName, String email) {
         this.fullName = fullName;
         this.email = email;
+
+        this.validateSelf();
     }
 
     public String getFullName() {
