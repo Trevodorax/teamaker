a2b1b9c91fc1d48cfeb0e900d19bef3b8cb60e9e
Anaelle EA <anaelle3364@gmail.com>
2023-12-17 23:41:57 +0100

    fix: add hiring date to developer entity

--- api-teamaker/src/main/java/org/teamaker/developer/domain/Developer.java
+++ api-teamaker/src/main/java/org/teamaker/developer/domain/Developer.java
@@ -1,11 +1,16 @@
 package org.teamaker.developer.domain;
 
+import java.util.Date;
+
 public class Developer {
     String fullName;
     String email;
 
-    public Developer(String fullName, String email) {
+    Date hiringDate;
+
+    public Developer(String fullName, String email, Date hiringDate) {
         this.fullName = fullName;
         this.email = email;
+        this.hiringDate = hiringDate;
     }
 }
