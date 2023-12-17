2412b4160ea47e374a90fc45036f92899db87073
Anaelle EA <anaelle3364@gmail.com>
2023-12-18 00:39:18 +0100

    add update developer port to developer repository

--- api-teamaker/src/main/java/org/teamaker/developer/adapter/out/persistence/DeveloperRepository.java
+++ api-teamaker/src/main/java/org/teamaker/developer/adapter/out/persistence/DeveloperRepository.java
@@ -1,7 +1,8 @@
 package org.teamaker.developer.adapter.out.persistence;
 
 import org.teamaker.developer.application.port.out.CreateDeveloperPort;
+import org.teamaker.developer.application.port.out.UpdateDeveloperPort;
 
-public interface DeveloperRepository extends CreateDeveloperPort {
+public interface DeveloperRepository extends CreateDeveloperPort, UpdateDeveloperPort {
 
 }
