d5b49c708151b66a70ce2877cbb337d442554c19
Anaelle EA <anaelle3364@gmail.com>
2023-12-17 23:18:41 +0100

    add hiring date

--- api-teamaker/src/main/java/org/teamaker/developer/application/port/out/CreateDeveloperCommand.java
+++ api-teamaker/src/main/java/org/teamaker/developer/application/port/out/CreateDeveloperCommand.java
@@ -3,17 +3,22 @@ package org.teamaker.developer.application.port.out;
 import org.teamaker.shared.validation.SelfValidating;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 public class CreateDeveloperCommand extends SelfValidating<CreateDeveloperCommand> {
     @NotNull
     private final String fullName;
     @Email
     private final String email;
+    @FutureOrPresent
+    private final Date hiringDate;
 
-    public CreateDeveloperCommand(String fullName, String email) {
+    public CreateDeveloperCommand(String fullName, String email, Date hiringDate) {
         this.fullName = fullName;
         this.email = email;
+        this.hiringDate = hiringDate;
 
         this.validateSelf();
     }
@@ -24,5 +29,9 @@ public class CreateDeveloperCommand extends SelfValidating<CreateDeveloperComman
 
     public String getEmail() {
         return email;
+    }
+
+    public Date getHiringDate() {
+        return hiringDate;
     }
 }
