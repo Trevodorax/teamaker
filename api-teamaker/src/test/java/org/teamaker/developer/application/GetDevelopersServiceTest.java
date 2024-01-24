aa3141ca33c13ac614605fe73ae6074bad637bfb
aea <aea@myges.fr>
2024-01-24 17:18:29 +0100

    fix(DevelopersServiceTest): add when in test

--- api-teamaker/src/test/java/org/teamaker/developer/application/GetDevelopersServiceTest.java
+++ api-teamaker/src/test/java/org/teamaker/developer/application/GetDevelopersServiceTest.java
@@ -2,6 +2,7 @@ package org.teamaker.developer.application;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.teamaker.developer.application.port.out.createDeveloper.CreateDeveloperCommand;
 import org.teamaker.developer.application.port.out.loadDevelopers.LoadDevelopersPort;
 import org.teamaker.developer.domain.Developer;
 import org.teamaker.developer.domain.dto.DeveloperResponse;
@@ -10,7 +11,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class GetDevelopersServiceTest {
     private static LoadDevelopersPort loadDevelopersPortMock;
@@ -25,13 +27,14 @@ public class GetDevelopersServiceTest {
     @Test
     public void testGetDevelopers() {
         LocalDate mockDate = LocalDate.now();
-        List<DeveloperResponse> mockDevelopersResponse = List.of(
-                new Developer("867GVC876a", "Developer fullName", "Developer email", mockDate).toResponse(),
-                new Developer("867GVC876b", "Developer fullName 2", "Developer email 2", mockDate).toResponse()
+        List<Developer> mockDevelopers = List.of(
+                new Developer("867GVC876a", "Developer fullName", "Developer email", mockDate),
+                new Developer("867GVC876b", "Developer fullName 2", "Developer email 2", mockDate)
         );
-
+        when(loadDevelopersPortMock.loadDevelopers()).thenReturn(mockDevelopers);
         List<DeveloperResponse> result = getDevelopersServiceMock.getDevelopers();
 
-        assertEquals(mockDevelopersResponse, result);
+        verify(loadDevelopersPortMock).loadDevelopers();
+        assertEquals(mockDevelopers.stream().map(Developer::toResponse).toList(), result);
     }
 }
