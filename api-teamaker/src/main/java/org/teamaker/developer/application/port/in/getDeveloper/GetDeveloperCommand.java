b7721ea7743e32f05b0660b139942c9aad4a0950
aea <aea@myges.fr>
2024-01-24 23:58:05 +0100

    MR feedback

--- api-teamaker/src/main/java/org/teamaker/developer/application/port/in/getDeveloper/GetDeveloperCommand.java
+++ api-teamaker/src/main/java/org/teamaker/developer/application/port/in/getDeveloper/GetDeveloperCommand.java
@@ -6,15 +6,15 @@ import javax.validation.constraints.NotNull;
 
 public class GetDeveloperCommand extends SelfValidating<GetDeveloperCommand> {
     @NotNull
-    private final String id;
+    private final String developerId;
 
-    public GetDeveloperCommand(String id) {
-        this.id = id;
+    public GetDeveloperCommand(String developerId) {
+        this.developerId = developerId;
 
         this.validateSelf();
     }
 
-    public String getId() {
-        return id;
+    public String getDeveloperId() {
+        return developerId;
     }
 }
