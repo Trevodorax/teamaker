b775be225b371a1f7ca4c7b8c635349c03e98c9a
Tom-Brd <bourlard.tom.94@gmail.com>
2024-01-24 11:58:12 +0100

    feat(getProjectsService): modify tests and service to handle response

--- api-teamaker/src/main/java/org/teamaker/project/application/port/out/loadProjects/LoadProjectsPort.java
+++ api-teamaker/src/main/java/org/teamaker/project/application/port/out/loadProjects/LoadProjectsPort.java
@@ -3,7 +3,12 @@ package org.teamaker.project.application.port.out.loadProjects;
 import org.teamaker.project.domain.Project;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public interface LoadProjectsPort {
+    /**
+     * Load all projects
+     * @return List of projects or empty list if no projects found
+     */
     List<Project> loadProjects();
 }
