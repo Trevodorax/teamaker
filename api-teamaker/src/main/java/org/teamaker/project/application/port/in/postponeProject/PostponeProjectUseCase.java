dd5c530f4fee4bb4545033cf99695116177ffcd4
Tom-Brd <bourlard.tom.94@gmail.com>
2024-01-24 14:33:22 +0100

    fix(postpone): add documentation on PostponeProjectUC

--- api-teamaker/src/main/java/org/teamaker/project/application/port/in/postponeProject/PostponeProjectUseCase.java
+++ api-teamaker/src/main/java/org/teamaker/project/application/port/in/postponeProject/PostponeProjectUseCase.java
@@ -3,5 +3,11 @@ package org.teamaker.project.application.port.in.postponeProject;
 import org.teamaker.project.domain.dto.PostponeProjectDtoResponse;
 
 public interface PostponeProjectUseCase {
+    /**
+     * Postpone a project
+     * @param command the command to postpone a project
+     * @return the project with the new dates
+     * @throws IllegalStateException if the project is not in a state that can be postponed
+     */
     PostponeProjectResponse.Response postponeProject(PostponeProjectCommand command) throws IllegalStateException;
 }
