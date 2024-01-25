70865994adc1120e33fa121faab010f536bf70cb
Tom-Brd <bourlard.tom.94@gmail.com>
2024-01-25 01:04:01 +0100

    chores(FindDevelopersByTechnologyPort): add documentation on port

--- api-teamaker/src/main/java/org/teamaker/developer/application/port/out/findDevelopersByTechnology/FindDevelopersByTechnologyPort.java
+++ api-teamaker/src/main/java/org/teamaker/developer/application/port/out/findDevelopersByTechnology/FindDevelopersByTechnologyPort.java
@@ -5,5 +5,10 @@ import org.teamaker.developer.domain.Developer;
 import java.util.List;
 
 public interface FindDevelopersByTechnologyPort {
+    /**
+     * Find developers by technology
+     * @param command technology id
+     * @return list of developers that use the technology or empty list if no developer uses the technology
+     */
     List<Developer> findDevelopersByTechnology(FindDevelopersByTechnologyCommand command);
 }
