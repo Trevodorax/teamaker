7137e1a8145954b08350ba7d25b692a51f41eb81
aea <aea@myges.fr>
2024-01-25 01:05:30 +0100

    get developer projects out port

--- api-teamaker/src/main/java/org/teamaker/developer/application/port/out/loadDeveloperProjects/LoadDeveloperProjectsPort.java
+++ api-teamaker/src/main/java/org/teamaker/developer/application/port/out/loadDeveloperProjects/LoadDeveloperProjectsPort.java
@@ -3,7 +3,8 @@ package org.teamaker.developer.application.port.out.loadDeveloperProjects;
 import org.teamaker.project.domain.Project;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public interface LoadDeveloperProjectsPort {
-    List<Project> loadDeveloperProjects(LoadDeveloperProjectsCommand command);
+    List<Project> loadDeveloperProjects(LoadDeveloperProjectsCommand command) throws NoSuchElementException;
 }
